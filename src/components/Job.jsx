f3dd007c6bb72846041d77802fda92584c672b58
Andrea Saccomanni <Andrea Saccomanni>
2024-12-11 08:45:37 +0100

    .

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,22 +1,40 @@
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Star, StarFill } from "react-bootstrap-icons";
-import { useSelector, useDispatch } from "react-redux";
-import { addToFavourite, removeToFavourite } from "../redux/actions";
+import { Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Star, StarFill } from 'react-bootstrap-icons'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  addToFavouriteAction,
+  removeFromFavouriteAction,
+} from '../redux/actions'
 
 const Job = ({ data }) => {
-  const favourites = useSelector((state) => state.favourite.list);
-  const dispatch = useDispatch();
+  const favourites = useSelector((state) => state.favourite.list)
+  const dispatch = useDispatch()
 
-  const isFav = favourites.includes(data.company_name);
+  const isFav = favourites.includes(data.company_name)
 
   return (
-    <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: '1px solid #00000033', borderRadius: 4 }}
+    >
       <Col xs={3}>
         {isFav ? (
-          <StarFill color="gold" size={16} className="mr-2 my-auto" onClick={() => dispatch(removeToFavourite(data))} />
+          <StarFill
+            color="gold"
+            size={16}
+            className="mr-2 my-auto"
+            onClick={() =>
+              dispatch(removeFromFavouriteAction(data.company_name))
+            }
+          />
         ) : (
-          <Star color="gold" size={16} className="mr-2 my-auto" onClick={() => dispatch(addToFavourite(data))} />
+          <Star
+            color="gold"
+            size={16}
+            className="mr-2 my-auto"
+            onClick={() => dispatch(addToFavouriteAction(data.company_name))}
+          />
         )}
         <Link to={`/${data.company_name}`}>{data.company_name}</Link>
       </Col>
@@ -26,7 +44,7 @@ const Job = ({ data }) => {
         </a>
       </Col>
     </Row>
-  );
-};
+  )
+}
 
-export default Job;
+export default Job
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,16 +1,34 @@
-export const ADD = "ADD_TO_FAVOURITE";
-export const REMOVE = "REMOVE_FROM_FAVOURITE";
+export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE'
+export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE'
+export const GET_JOBS = 'GET_JOBS'
 
-export const addToFavourite = (data) => {
-  return {
-    type: ADD,
-    payload: data.company_name
-  };
-};
+const baseEndpoint = 'https://strive-benchmark.herokuapp.com/api/jobs?search='
 
-export const removeToFavourite = (data) => {
-  return {
-    type: REMOVE,
-    payload: data.company_name
-  };
-};
+export const addToFavouriteAction = (company) => ({
+  type: ADD_TO_FAVOURITE,
+  payload: company,
+})
+
+export const removeFromFavouriteAction = (company) => ({
+  type: REMOVE_FROM_FAVOURITE,
+  payload: company,
+})
+
+export const getJobsAction = (query) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(baseEndpoint + query + '&limit=20')
+      if (response.ok) {
+        const { data } = await response.json()
+        dispatch({
+          type: GET_JOBS,
+          payload: data,
+        })
+      } else {
+        alert('Error fetching results')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
